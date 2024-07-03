c54bd106e2e41b75ae1d96102e979237f805dc09
BoyoungH <hyong0903@naver.com>
2024-07-03 21:27:56 +0900

    Add: History Entity 작성

--- src/main/java/com/prizethree/roleplay/entity/History.java
+++ src/main/java/com/prizethree/roleplay/entity/History.java
@@ -8,5 +8,34 @@ import java.util.*;
 
 @Entity
 public class History {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "voice_id")
+    private Voice voice;
+
+    @Column(name = "situation", length = 100)
+    private String situation;
+
+    @Column(name = "duration")
+    private LocalDateTime duration;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Column(name = "my_role", length = 100)
+    private String myRole;
+
+    @Column(name = "ai_role", length = 100)
+    private String aiRole;
+
+    @OneToMany(mappedBy = "history", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Tag> tags;
 
 }
