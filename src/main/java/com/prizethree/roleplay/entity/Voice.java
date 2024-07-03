5bf90090a0415df5ddf0511bb3c1b02ce2187a9e
BoyoungH <hyong0903@naver.com>
2024-07-03 21:28:16 +0900

    Add: Voice Entity 작성

--- src/main/java/com/prizethree/roleplay/entity/Voice.java
+++ src/main/java/com/prizethree/roleplay/entity/Voice.java
@@ -5,5 +5,15 @@ import jakarta.persistence.*;
 
 @Entity
 public class Voice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "voice_name", nullable = false, length = 50)
+    private String voiceName;
 
 }
