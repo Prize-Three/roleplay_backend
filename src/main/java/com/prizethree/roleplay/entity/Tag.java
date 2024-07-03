770536b4c8a94cf146a6069f3f35fb0e21063da6
BoyoungH <hyong0903@naver.com>
2024-07-03 21:27:36 +0900

    Add: Tag Entity 작성

--- src/main/java/com/prizethree/roleplay/entity/Tag.java
+++ src/main/java/com/prizethree/roleplay/entity/Tag.java
@@ -3,5 +3,15 @@ package com.prizethree.roleplay.entity;
 import jakarta.persistence.*;
 @Entity
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "history_id", nullable = false)
+    private History history;
+
+    @Column(name = "tag", nullable = false, length = 100)
+    private String tag;
 
 }
